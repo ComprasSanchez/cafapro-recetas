2f2ba1d51521c9edc213c7794e1c0cde2b65ceb7
Joaquin Tissera <tisserajoaquin@gmail.com>
2026-02-03 12:32:50 -0300

    se modifoc el orden en que develve la informacion

--- app/service/auditoria/view_auditoria.py
+++ app/service/auditoria/view_auditoria.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from sqlalchemy import select
+from typing import Any, Sequence
+
+from sqlalchemy import select, Row, RowMapping
 from sqlalchemy.orm import Session
 
 from app.db.view import VwArchivoResumenAuditoria
@@ -8,10 +10,11 @@ from app.db.view import VwArchivoResumenAuditoria
 
 class ViewAuditoriaService:
     @staticmethod
-    def list(s: Session, recepcion_id: int) -> list[VwArchivoResumenAuditoria]:
+    def list(s: Session, recepcion_id: int) -> Sequence[VwArchivoResumenAuditoria]:
         stmt = (
             select(VwArchivoResumenAuditoria)
             .where(VwArchivoResumenAuditoria.recepcion_id == recepcion_id)
+            .order_by(VwArchivoResumenAuditoria.frente_jpg.asc())
         )
         return s.execute(stmt).scalars().all()
 
--- installer.iss
+++ installer.iss
@@ -1,6 +1,6 @@
 [Setup]
 AppName=CafaproRecetas
-AppVersion=1.0.12-beta
+AppVersion=1.0.13-beta
 DefaultDirName={pf}\CafaproRecetas
 DefaultGroupName=CafaproRecetas
 OutputDir=output
